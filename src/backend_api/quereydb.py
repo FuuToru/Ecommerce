8169d0439245171f37dce1012ab554f0a2f96af9
Tri <huutri231103@gmail.com>
2024-05-26 23:13:04 +0700

    add aws s3

--- src/backend_api/quereydb.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('/Users/fuutoru/Library/Mobile Documents/com~apple~CloudDocs/Study/A-Learn_st_III/HK2/Phát triển ứng dụng/test-project/backend_api/db.sqlite3')
-cursor = conn.cursor()
-cursor.execute("DELETE FROM main_orderitems;")
-conn.commit()
-conn.close()
-print("Success.")
